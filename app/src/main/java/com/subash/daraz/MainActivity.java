443d080cf4228b76b9abed22b1ef5a39e8cef1f5
Subash2171 <subashgurung217@gmail.com>
2020-01-01 20:56:53 +0545

    login

---
+++ app/src/main/java/com/subash/daraz/MainActivity.java
@@ -0,0 +1,14 @@
+package com.subash.daraz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
